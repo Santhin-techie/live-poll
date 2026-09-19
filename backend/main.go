17987b37e83d8c4a61c447e3a5eb25d5ff2ae458
Santhin-techie <santhinkumar739@gmail.com>
2026-09-19 12:05:11 +0530

    allow PATCH in CORS for close poll

--- backend/main.go
+++ backend/main.go
@@ -29,7 +29,7 @@ func main() {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{cfg.FrontendURL},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
 	}))
